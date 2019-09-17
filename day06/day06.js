c6764d2b1ad863ed19fdbd736964663f95592dbd
git-sunyingzhan <oneselfwechat@163.com>
2019-09-17 12:01:00 +0800

    内置函数

---
+++ day06/day06.js
@@ -0,0 +1,28 @@
+//Number对象
+var age = 10;
+console.log(age);
+console.log(typeof age);
+var salary = new Number(2000);
+console.log(salary);
+console.log(typeof salary);
+
+
+//常用函数
+var num = 45.557;
+console.log(num.toFixed(0));
+console.log(typeof num.toFixed(0));
+
+
+//String对象
+var str1 = "abc";
+console.log(str1);
+console.log(typeof str1);
+var str2 = new String("ABC");
+console.log(str2);
+console.log(typeof str2);
+
+
+//常用属性和函数
+console.log(str2.length);
+console.log(str2.toLocaleUpperCase());
+console.log(str2.charAt(1))
